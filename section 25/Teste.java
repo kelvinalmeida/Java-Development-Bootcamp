64bb13daba920904e9c75278bad7e1148eb3e6a0
Kelvin <kelvinsantosdealmeida@gmail.com>
2024-04-29 19:03:11 -0300

    section 26

---
+++ section 25/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) {
+        String name = "Kelvin";
+        String name2 = name;
+
+        name2 = "Bia";
+    }
+}
---
+++ section 26/Part 2/CityPopulationTracker.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class CityPopulationTracker {
+
+        private HashMap<String, City> cityPopulations;
+
+
+        public CityPopulationTracker() {
+            cityPopulations = new HashMap<>();
+        }
+
+
+        public City getCity(String cityName) {
+            
+            City city = cityPopulations.get(cityName);
+
+            return new City(city);
+        }
+
+        public void setCity(City city) {
+            cityPopulations.put(city.getName(), new City(city));
+        }
+
+        public void addCity(City city) {
+            cityPopulations.put(city.getName(), new City(city));
+        }
+
+}
+
+
+
